bcf180465a11d940291f6e6271af97b49d43f9b9
pan210937-afk <pan210937@gmail.com>
2026-02-20 20:57:08 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME="ndss-cache-v1";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./data.json","./manifest.json","./sw.js","./pricing.html","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener("fetch",(e)=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return resp;}).catch(()=>cached)));});
